35402a64c92e6c482c6c26836d16e131b89ddcdd
Dhiraj <bdhiraj132006@gmail.com>
2026-09-23 21:17:55 +0530

    Time: 1 ms (83.67%), Space: 42.7 MB (74.52%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+        int k=0;
+
+        for(char c:s.toCharArray())
+        {
+            if(Character.isDigit(c))
+            {
+                k=k*10+(c-'0');
+            }
+            else if(c=='[')
+            {
+                countStack.push(k);
+                stringStack.push(current);
+                k=0;
+                current=new StringBuilder();
+            }
+            else if(c==']')
+            {
+                int repeatCount=countStack.pop();
+                StringBuilder decodeString=stringStack.pop();
+                for(int i=0;i<repeatCount;i++)
+                {
+                    decodeString.append(current);
+                }
+                current=decodeString;
+            }
+            else
+            {
+                current.append(c);
+            }
+        }
+        return current.toString();
+    }
+}
